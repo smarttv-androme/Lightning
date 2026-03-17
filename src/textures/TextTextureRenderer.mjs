436f914a397cbb089c3d3df383a1806fa7463b5a
sem <sem.heleven@androme.be>
2026-03-17 09:23:47 +0100

    Fallback to default font metrics if a font face is not specified

--- src/textures/TextTextureRenderer.mjs
+++ src/textures/TextTextureRenderer.mjs
@@ -462,7 +462,7 @@ export default class TextTextureRenderer {
      * @return {Stage.FontMetrics | undefined}
      */
     _getFontMetrics() {
-        const fontMetrics = this._stage.fontMetrics[this._settings.fontFace];
+        const fontMetrics = this._stage.fontMetrics[this._settings.fontFace] ?? this._stage.fontMetrics[this._stage.getOption('defaultFontFace')];
 
         if (!fontMetrics) {
             console.warn(`[Lightning] No font metrics found for font '${this._settings.fontFace}'!`);
